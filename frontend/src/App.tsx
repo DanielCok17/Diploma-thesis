35ff2990f2a804c2b294207b4a29e4045ff7743a
Daniel Cok <Daniel.Cok@onsemi.com>
2023-02-26 00:43:26 +0100

    Sidebar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Map from "./components/Map";
+import Sidebar from "./components/Sidebar";
+import { FC } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const App = () => {
+const App: FC = () => {
   return (
-    <div className="App">
-      <Map />
-    </div>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path="/map" component={Map} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const Nav = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 5rem;
+    background-color: black;
+`;
+
+const SidebarNav = styled.div<{ sidebar: boolean }>`
+    width: 250px;
+    height: 100vh;
+    background-color: black;
+    position: fixed;
+    top: 0;
+    left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+    transition: 350ms;
+`;
+
+const NavIcon = styled(Link)`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 5rem;
+    font-size: 2rem;
+    margin-left: 2rem;
+`;
+
+const SidebarWrap = styled.div``;
+
+const Sidebar: FC = () => {
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+
+    return (
+        <IconContext.Provider value={{ color: '#fff' }}>
+            <Nav>
+                <NavIcon to="#" onClick={showSidebar}>
+                    <AiOutlineMenu />
+                </NavIcon>
+            </Nav>
+            <SidebarNav sidebar={sidebar}>
+                <SidebarWrap>
+                    <NavIcon to="#" onClick={showSidebar}>
+                        <AiOutlineClose />
+                    </NavIcon>
+                </SidebarWrap>
+            </SidebarNav>
+        </IconContext.Provider>
+    );
+};
+
+export default Sidebar;
+
