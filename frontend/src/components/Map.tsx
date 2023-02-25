8502ee663b778a2c533312842896e95ee9610aed
Daniel Cok <Daniel.Cok@onsemi.com>
2023-02-25 19:44:37 +0100

    Map component added

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useMemo, useState } from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import "../App.css";
+
+const googleMapsApiKey: string = process.env.REACT_APP_GOOGLE_API_KEY || "";
+const env_lat: number = Number(process.env.REACT_APP_LAT) || 0;
+const env_lng: number = Number(process.env.REACT_APP_LNG) || 0;
+
+const Map = () => {
+  const [latitute, setLatitute] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey,
+  });
+  const center = useMemo(() => ({ lat: latitute || env_lat, lng: longitude || env_lng }), []);
+
+  // Get the user's current location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitute(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="App">
+      {!isLoaded ? (
+        <h1>Loading map...</h1>
+      ) : (
+        <GoogleMap mapContainerClassName="map-container" center={center} zoom={10} />
+      )}
+    </div>
+  );
+};
+
+export default Map;
